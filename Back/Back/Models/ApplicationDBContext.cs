4bd3ecbe7be2784c2b5272cf70bfcaaf92b30274
Ivan Oparin <ivanopa.rin@yandex.ru>
2025-07-20 15:42:36 +0300

    adding db support

---
+++ Back/Back/Models/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Back.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Back/Back/Program.cs
+++ Back/Back/Program.cs
@@ -1,4 +1,7 @@
 
+using Back.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Back
 {
     public class Program
@@ -6,24 +9,29 @@ namespace Back
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
-            // Add services to the container.
+            //      DB_CONFIGURATION          //
+            string host = configuration["POSTGRES_HOST"]!;
+            string port = configuration["POSTGRES_PORT"]!;
+            string database = configuration["POSTGRES_DB"]!;
+            string username = configuration["POSTGRES_USER"]!;
+            string password = configuration["POSTGRES_PASSWORD"]!;
+            string conStr = $"Host={host};Port={port};Database={database};Username={username};Password={password}";
+           
+            builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseNpgsql(conStr));
+            /////////////////////////////////////
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
